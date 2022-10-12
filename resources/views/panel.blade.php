d49cc0fdd54ed4d0008e8d2de47f6f50c285405d
Mihai Vescan <vescmihai@gmail.com>
2022-10-12 15:14:50 -0400

    Autenticacion farmacia

---
+++ resources/views/panel.blade.php
@@ -0,0 +1,62 @@
+@extends('adminlte::page')
+
+@section('title', 'Farmacia Arcoiris')
+
+@section('content_header')
+    <h1>Inicio</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Ultima conexión</h1>
+        </div>
+
+        <div class="card-body">
+            <p>Bienvenido Mihai, te vimos por ultima vez el 10/10/2022 a las 17:12.
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Productos</h1>
+        </div>
+
+        <div class="card-body">
+            <p>Producto 1
+        </div>
+        <div class="card-body">
+            <p>Producto 2
+        </div>
+        <div class="card-body">
+            <p>Producto 3
+        </div>
+        <div class="card-body">
+            <p>Producto 4
+        </div>
+        <div class="card-body">
+            <p>Producto 5
+        </div>
+        <div class="card-body">
+            <p>Producto 6
+        </div>
+        <div class="card-body">
+            <p>Producto 7
+        </div>
+        <div class="card-body">
+            <p>Producto 2
+        </div>
+        <div class="card-body">
+            <p>Producto 3
+        </div>
+        <div class="card-body">
+            <p>Producto 4
+        </div>
+        <div class="card-body">
+            <p>Producto 5
+        </div>
+        <div class="card-body">
+            <p>Producto 6
+        </div>
+    </div>
+@stop
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/panel', function () {
+    return view('panel');
+})->middleware(['auth', 'verified'])->name('panel');
+
+require __DIR__.'/auth.php';
